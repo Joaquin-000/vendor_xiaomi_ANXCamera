3df994925bd882d62fa44ee6f4f680182dee750b
Prince Cooper <pcooper7736@gmail.com>
2021-09-14 23:31:39 +0530

    mojito: Decommonize 48mp fix
    We already have stock cameraservice on vendor
    
    Signed-off-by: Prince Cooper <pcooper7736@gmail.com>

--- config.mk
+++ config.mk
@@ -16,10 +16,7 @@ VENDOR_PATH := vendor/xiaomi/ANXCamera
 
 PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/priv-app/ANXCamera/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/ANXCamera/lib) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/48mp_fix/bin,$(TARGET_COPY_OUT_SYSTEM)/bin) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/48mp_fix/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/48mp_fix/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64)
+	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/priv-app/ANXCamera/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/ANXCamera/lib)
 
 
 PRODUCT_PACKAGES += \
